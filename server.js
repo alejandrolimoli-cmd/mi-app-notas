09dafcd6e09e8d03f49392488ed44844e7d6f010
Alejandro Limoli <alejandrolimoli@gmail.com>
2026-03-20 23:53:38 -0300

    Actualizar con eliminación de grupos y UI mejorada

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const { MongoClient, ObjectId } = require('mongodb');
 const bcrypt = require('bcryptjs');
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
